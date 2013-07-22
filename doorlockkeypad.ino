da14e2fee52a5724e4de1a2a23bbc388df261832
Gregory Short <gshort@alaska.edu>
2013-07-22 11:49:29 -0400

    Changed some alert patterns. Moved the led strip off the hardware spi pins, and adjusted the keypad pins to not interfere. Added a once-per-ten-seconds fake auth process for testing without a keypad hooked up.

--- doorlockkeypad.ino
+++ doorlockkeypad.ino
@@ -29,16 +29,18 @@ char keys[rows][cols] = {
   {'7','8','9'},
   {'#','0','*'}
 };
-byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[rows] = {A3, A2, A1, A0}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {4, 3, 2}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
 String code = "";
 int codestart = 0;
 
 void setup() {
-  //LEDS.addLeds<WS2801>(leds, NUM_LEDS); //can't use the SPI pins with the ethernet shield in place
-  LEDS.addLeds<WS2801, A0, A1, BGR, DATA_RATE_MHZ(1)>(leds, NUM_LEDS); //use pins A0, A1 in software mode...we're only driving 12 LEDs anyhow
+//  LEDS.addLeds<WS2801>(leds, NUM_LEDS); //can't use the SPI pins with the ethernet shield in place
+  LEDS.addLeds<WS2801, 5, 7, BGR>(leds, NUM_LEDS); //use non-spi pins in software mode...we're only driving 12 LEDs anyhow
+  LEDS.showColor(CRGB(255, 100, 0)); //boot status color
+  
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect
@@ -47,7 +49,7 @@ void setup() {
     Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
   }
-  delay(1000);
+  delay(1000); //need a delay here for a quirky library or two
 }
 
 void loop() {
@@ -76,8 +78,14 @@ void loop() {
       code += key;
     }
   }
-  authorize("1234");
-  while(true) {  1;  }
+  if(millis() % 10000 == 0) {
+    LEDS.showColor(CRGB(0, 255, 255));
+    if(authorize("1596")) {
+      alert_allow();
+    } else {
+      alert_deny();
+    }
+  }
 }
 
 boolean authorize(String code) { //given a PIN, check authorization against web server
@@ -132,13 +140,13 @@ boolean check_auth_result(String json) {
 
 void status_idling() { //slowly "breathe" blue, deterministic: call as often as convenient (or possible)
   if(millis() % 10 == 0) {
-    LEDS.showColor(CRGB(0, 0, floor((sin(float(millis()) / 4.0) * 0.5 + 1.0) * 255)));
+    LEDS.showColor(CRGB(0, 0, floor((sin(float(millis()) / 1000.0) * 0.5 + 0.5) * 255)));
   }
 }
 
 void status_reading() { //quickly "breathe" yellow, deterministic: call as often as convenient (or possible)
   if(millis() % 10 == 0) {
-    int v = floor((sin(float(millis()) / 2.0) * 0.5 + 1.0) * 255);
+    int v = floor((sin(float(millis()) / 100.0) * 0.5 + 0.5) * 255);
     LEDS.showColor(CRGB(v, v, 0));
   }
 }
@@ -151,16 +159,24 @@ void status_thinking() { //spin a rainbow, deterministic: call as often as conve
 }
 
 void alert_deny() { //flash red three times and fade out after the third, blocking: takes roughly 1 second total
-  LEDS.showColor(CRGB(255, 0, 0));
-  delay(200);
-  LEDS.showColor(CRGB(0, 0, 0));
+  LEDS.showColor(CRGB(255, 0, 255));
   delay(100);
-  LEDS.showColor(CRGB(255, 0, 0));
-  delay(200);
   LEDS.showColor(CRGB(0, 0, 0));
+  delay(50);
+  LEDS.showColor(CRGB(255, 0, 255));
   delay(100);
+  LEDS.showColor(CRGB(0, 0, 0));
+  delay(50);
+  LEDS.showColor(CRGB(255, 0, 255));
+  delay(100);
+  LEDS.showColor(CRGB(0, 0, 0));
+  delay(50);
   LEDS.showColor(CRGB(255, 0, 0));
-  delay(200);
+  delay(100);
+  LEDS.showColor(CRGB(0, 0, 0));
+  delay(50);
+  LEDS.showColor(CRGB(255, 0, 0));
+  delay(100);
   for(int ii = 255; ii >= 0; ii-=2) {
     LEDS.showColor(CRGB(ii, 0, 0));
     delay(10);
@@ -168,6 +184,10 @@ void alert_deny() { //flash red three times and fade out after the third, blocki
 }
 
 void alert_allow() { //illuminate green and fade out after a bit, blocking: takes roughly 5 seconds total
+  LEDS.showColor(CRGB(0, 255, 0));
+  delay(100);
+  LEDS.showColor(CRGB(0, 0, 0));
+  delay(50);
   LEDS.showColor(CRGB(0, 255, 0));
   delay(2500);
   for(int ii = 255; ii >= 0; --ii) {
